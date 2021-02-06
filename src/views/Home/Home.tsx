5d7785e4bd0af6df42af7ab3e689abcc97a1b43a
ape-swap <ape.swap@outlook.com>
2021-02-06 14:39:03 -0300

    refactor: Rename home

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -83,9 +83,9 @@ const Home: React.FC = () => {
     <Page>
       <Hero>
         <Heading as="h1" size="xl" mb="24px" color="secondary">
-          {TranslateString(576, 'PancakeSwap')}
+          {TranslateString(576, 'ApeSwap')}
         </Heading>
-        <Text>{TranslateString(578, 'The #1 AMM and yield farm on Binance Smart Chain.')}</Text>
+        <Text>{TranslateString(578, 'The #1 AMM and yield farm for apes by apes.')}</Text>
       </Hero>
       <div>
         <Cards>
