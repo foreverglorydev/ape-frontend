6ab95740bcd58bb5bd429b6f80cf831e683f8d15
ape-swap <ape.swap@hotmail.com>
2021-02-06 18:06:27 -0300

    style: Change favicon

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -94,7 +94,7 @@ const Home: React.FC = () => {
         </Cards>
         <Cards>
           <EarnAPYCard />
-          { /* <EarnAssetCard /> */ }
+          {/* <EarnAssetCard /> */}
           <TotalValueLockedCard />
         </Cards>
       </div>
