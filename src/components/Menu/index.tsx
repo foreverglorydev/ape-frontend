35d0b483e0122d1a36ce02be5b47ea0b337b2a55
ape-swap <ape.swap@hotmail.com>
2021-02-11 23:10:07 -0300

    style: Banana pools styling

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react'
 import { Menu as UikitMenu } from '@apeswapfinance/uikit'
 import { useWallet } from '@binance-chain/bsc-use-wallet'
-import { allLanguages } from 'config/localisation/languageCodes'
 import { LanguageContext } from 'contexts/Localisation/languageContext'
 import useTheme from 'hooks/useTheme'
 import { usePriceCakeBusd } from 'state/hooks'
