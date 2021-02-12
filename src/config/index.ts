ebdc12a11d492b30634ccb2ab59c45eb97440487
ape-swap <ape.swap@hotmail.com>
2021-02-12 20:48:54 -0300

    refactor: Base exchange url to react variable

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,6 +9,6 @@ export const CAKE_PER_BLOCK = new BigNumber(40)
 export const BLOCKS_PER_YEAR = new BigNumber(10512000)
 export const BSC_BLOCK_TIME = 3
 export const BANANA_POOL_PID = 3
-export const BASE_EXCHANGE_URL = process.env.BASE_EXCHANGE_URL || 'https://apeswap-dex.vercel.app'
+export const BASE_EXCHANGE_URL = process.env.REACT_APP_BASE_EXCHANGE_URL || 'https://apeswap-dex.vercel.app'
 export const BASE_ADD_LIQUIDITY_URL = `${BASE_EXCHANGE_URL}/#/add`
 export const BASE_LIQUIDITY_POOL_URL = `${BASE_EXCHANGE_URL}/#/pool`
