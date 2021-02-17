81b26aee4634e1090276a932d43e6a2d4c1a36cc
ape-swap <ape.swap@hotmail.com>
2021-02-16 23:23:28 -0300

    chore: Update banana per block

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,7 @@ BigNumber.config({
   DECIMAL_PLACES: 80,
 })
 
-export const CAKE_PER_BLOCK = new BigNumber(40)
+export const CAKE_PER_BLOCK = new BigNumber(20)
 export const BLOCKS_PER_YEAR = new BigNumber(10512000)
 export const BSC_BLOCK_TIME = 3
 export const BANANA_POOL_PID = 1
