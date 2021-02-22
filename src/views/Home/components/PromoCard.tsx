7c8b24502f89fe0277acb6f935785953a3e837ef
ape-swap <ape.swap@hotmail.com>
2021-02-22 18:29:31 -0300

    chore: Lint fixes

--- src/views/Home/components/PromoCard.tsx
+++ src/views/Home/components/PromoCard.tsx
@@ -26,23 +26,31 @@ const PromoCard = () => {
   const TranslateString = useI18n()
   return (
     <StyledPromoCard>
-      <a href="https://ape-swap.medium.com/apeswap-design-contest-500-banana-prize-273da4126f5b"  target="_blank" rel="noreferrer">
-      <CardBody>
-        <Heading size="xl" mb="24px">
-        🍌 {TranslateString(999, 'Calling all ape designers!')} 🍌
-        </Heading>
-        <>
-          <Text color="textSubtle">
-            We are running a design contest to enhance ApeSwaps overall look and feel!
-          </Text>
-          <Text color="textSubtle">
-            Over 1000 <StyledImage src="/images/tokens/BANANA.svg" width={16} height={16} alt="BANANA" /> $BANANA in prices
-            <StyledLink href="https://ape-swap.medium.com/apeswap-design-contest-500-banana-prize-273da4126f5b" target="_blank"> Check it out!</StyledLink>
-          </Text>
-        </>
-      </CardBody>
+      <a
+        href="https://ape-swap.medium.com/apeswap-design-contest-500-banana-prize-273da4126f5b"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <CardBody>
+          <Heading size="xl" mb="24px">
+            🍌 {TranslateString(999, 'Calling all ape designers!')} 🍌
+          </Heading>
+          <>
+            <Text color="textSubtle">We are running a design contest to enhance ApeSwaps overall look and feel!</Text>
+            <Text color="textSubtle">
+              Over 1000 <StyledImage src="/images/tokens/BANANA.svg" width={16} height={16} alt="BANANA" /> $BANANA in
+              prices
+              <StyledLink
+                href="https://ape-swap.medium.com/apeswap-design-contest-500-banana-prize-273da4126f5b"
+                target="_blank"
+              >
+                {' '}
+                Check it out!
+              </StyledLink>
+            </Text>
+          </>
+        </CardBody>
       </a>
-      
     </StyledPromoCard>
   )
 }
