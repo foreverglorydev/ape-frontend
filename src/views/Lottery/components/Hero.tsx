1ef10ed048936d0278ce1d1e9a7c54f5bb408b04
ape-swap <ape.swap@hotmail.com>
2021-03-23 01:21:16 -0300

    chore: Format fixes

--- src/views/Lottery/components/Hero.tsx
+++ src/views/Lottery/components/Hero.tsx
@@ -6,7 +6,7 @@ import Container from 'components/layout/Container'
 import LotteryProgress from './LotteryProgress'
 
 const Title = styled(Heading).attrs({ as: 'h1', size: 'xl' })`
-  color: #FFFFFF;
+  color: #ffffff;
   margin-bottom: 24px;
 `
 
