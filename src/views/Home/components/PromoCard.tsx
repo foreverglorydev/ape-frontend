2b9c10e66d092571b44302d716823e66bad9957f
ape-swap <ape.swap@hotmail.com>
2021-04-03 00:55:08 -0300

    chore: Lint fix

--- src/views/Home/components/PromoCard.tsx
+++ src/views/Home/components/PromoCard.tsx
@@ -26,15 +26,27 @@ const PromoCard = () => {
   const TranslateString = useI18n()
   return (
     <StyledPromoCard>
-      <a href="https://ape-swap.medium.com/introducing-the-apeswap-buidl-program-%EF%B8%8F-bf1d6c74e145" target="_blank" rel="noreferrer">
+      <a
+        href="https://ape-swap.medium.com/introducing-the-apeswap-buidl-program-%EF%B8%8F-bf1d6c74e145"
+        target="_blank"
+        rel="noreferrer"
+      >
         <CardBody>
           <Heading size="lg" mb="24px">
             ⚒️ Introducing the ApeSwap BUIDL Program ⚒️
           </Heading>
           <>
-            <Text color="textSubtle">If you are an up and coming project on BSC you can apply to get into the jungle</Text>
             <Text color="textSubtle">
-              <StyledLink href="https://ape-swap.medium.com/introducing-the-apeswap-buidl-program-%EF%B8%8F-bf1d6c74e145" target="_blank" rel="noreferrer">Come grow with us 🚀</StyledLink>
+              If you are an up and coming project on BSC you can apply to get into the jungle
+            </Text>
+            <Text color="textSubtle">
+              <StyledLink
+                href="https://ape-swap.medium.com/introducing-the-apeswap-buidl-program-%EF%B8%8F-bf1d6c74e145"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Come grow with us 🚀
+              </StyledLink>
             </Text>
           </>
         </CardBody>
