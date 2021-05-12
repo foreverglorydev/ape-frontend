f2efe0bed5b718033362a951c880870cc58aa923
ape-swap <ape.swap@hotmail.com>
2021-05-12 15:58:25 -0300

    chore: Lint fixes

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -11,22 +11,22 @@ interface SizeProps {
 }
 
 const StyledPage = styled(Container)<SizeProps>`
-min-height: calc(100vh - 64px);
-padding-top: 16px;
-padding-bottom: 16px;
-margin-left: auto;
-margin-right: auto;
+  min-height: calc(100vh - 64px);
+  padding-top: 16px;
+  padding-bottom: 16px;
+  margin-left: auto;
+  margin-right: auto;
 
-${({ theme }) => theme.mediaQueries.sm} {
-  padding-top: 24px;
-  padding-bottom: 24px;
-}
+  ${({ theme }) => theme.mediaQueries.sm} {
+    padding-top: 24px;
+    padding-bottom: 24px;
+  }
 
-${({ theme }) => theme.mediaQueries.lg} {
-  padding-top: 32px;
-  padding-bottom: 32px;
-  max-width: ${({ width }) => width || '992px'};
-}
+  ${({ theme }) => theme.mediaQueries.lg} {
+    padding-top: 32px;
+    padding-bottom: 32px;
+    max-width: ${({ width }) => width || '992px'};
+  }
 `
 
 const PageMeta = () => {
