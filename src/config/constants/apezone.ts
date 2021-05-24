5c29d3b8135bc077b642fb405370e513a38eab02
ape-swap <ape.swap@hotmail.com>
2021-05-24 16:13:39 -0300

    feat: Update IAO contracts

--- src/config/constants/apezone.ts
+++ src/config/constants/apezone.ts
@@ -21,7 +21,7 @@ const ApeZone = {
     },
     {
       id: 'aperocket',
-      address: '0x18Bf00A9F7e3dF1ec2f9c4A77658b0797A90Db19',
+      address: '0xf07cb1C18eBF84570A59cE13365d504279db2c8E',
       isActive: true,
       name: 'ApeRocket',
       subTitle: 'DeFi yield farming aggregator and optimizer for Binance Smart Chain.',
@@ -37,7 +37,6 @@ const ApeZone = {
       currencyAddress: '0xddb3bd8645775f59496c821e4f55a7ea6a6dc299',
       tokenDecimals: 18,
       releaseBlockNumber: 7378325,
-      startBlock: 7705200,
       // burnedTxUrl: 'https://bscscan.com/tx/0x938454e722fdef0a2f34b16f16bed50f6deb692b942331a9a6e2cf96977e116b',
     },
   ],
