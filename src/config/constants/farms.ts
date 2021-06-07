8c062433802d05a8594403050c2f1538ad6c48a8
Harambe-Nakamoto <harambeN@protonmail.com>
2021-06-07 09:38:19 -0500

    feat: List space farm

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -879,6 +879,21 @@ const farms: FarmConfig[] = [
     quoteTokenAdresses: contracts.wbnb,
   },
   {
+    pid: 64,
+    lpSymbol: 'SPACE-BNB LP',
+    lpAddresses: {
+      97: '0xed89477d619c7e73f752d5fc7be60308ceb63663',
+      56: '0xd0f82498051067e154d1dcd3d88fa95063949d7e',
+    },
+    tokenSymbol: 'SPACE',
+    tokenAddresses: {
+      97: '0x4Fb99590cA95fc3255D9fA66a1cA46c43C34b09a',
+      56: '0xe486a69e432fdc29622bf00315f6b34c99b45e80',
+    },
+    quoteTokenSymbol: QuoteToken.BNB,
+    quoteTokenAdresses: contracts.wbnb,
+  },
+  {
     pid: 7, // NOTE: Inactive Farm
     lpSymbol: 'BANANA-BAKE LP',
     lpAddresses: {
