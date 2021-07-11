7daf40c2478a3a74823bc81abc37e9c1a5f594a9
obiedobo <obiedobo@gmail.com>
2021-07-11 15:16:33 -0600

    feat: Added NALIS pool

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -1403,6 +1403,28 @@ const pools: PoolConfig[] = [
     isFinished: false,
     tokenDecimals: 18,
   },
+  {
+    sousId: 64,
+    tokenName: 'NALIS',
+    image: 'NALIS.svg',
+    stakingTokenName: 'GNANA',
+    stakingTokenAddress: {
+      97: '0x4fb99590ca95fc3255d9fa66a1ca46c43c34b09a',
+      56: '0xddb3bd8645775f59496c821e4f55a7ea6a6dc299',
+    },
+    contractAddress: {
+      97: '0x6c6248ea3ff05200b9df0bc91cec2dea4f57a95d',
+      56: '0x063e98d9f0484c07028eb78661df554a064aeb05',
+    },
+    poolCategory: PoolCategory.APEZONE,
+    projectLink: 'https://koaladefi.finance/',
+    harvest: true,
+    tokenPerBlock: `0.028935185185185185`,
+    sortOrder: 2,
+    reflect: true,
+    isFinished: false,
+    tokenDecimals: 18,
+  },
   /* {
     sousId: 45,
     tokenName: 'LINA',
