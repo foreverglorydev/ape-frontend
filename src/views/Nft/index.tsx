3defe912602f2a82df1e5ad1469b24b577656d7f
harambe-nakamoto <harambeN@protonmail.com>
2021-07-18 15:52:33 -0500

    chore: Fix click issue

--- src/views/Nft/index.tsx
+++ src/views/Nft/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Text  } from '@apeswapfinance/uikit'
+import { Text } from '@apeswapfinance/uikit'
 import useI18n from 'hooks/useI18n'
 import Page from 'components/layout/Page'
 import nfts from 'config/constants/nfts'
