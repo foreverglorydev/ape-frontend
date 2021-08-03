8f68f82b030e779b1b0ba67da51242e45a735e07
Obie Dobo <78942906+obiedobo@users.noreply.github.com>
2021-08-03 15:01:48 -0600

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -1426,7 +1426,7 @@ const farms: FarmConfig[] = [
       97: '0xed89477d619c7e73f752d5fc7be60308ceb63663',
       56: '0xaa3fcba2cf7c5f8c8f785a7180f5063144fe53c6',
     },
-    tokenSymbol: 'LAND',
+    tokenSymbol: 'POTS',
     tokenAddresses: {
       97: '0x4Fb99590cA95fc3255D9fA66a1cA46c43C34b09a',
       56: '0x3fcca8648651e5b974dd6d3e50f61567779772a8',
