be1711d5d6ae8cdf7e1dd1e3c8241fc375c7f7cd
Obie Dobo <obiedobo@gmail.com>
2021-08-18 17:58:55 -0500

    feat: BABI farm and pool

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -1804,6 +1804,28 @@ const pools: PoolConfig[] = [
     isFinished: false,
     tokenDecimals: 18,
   },
+  {
+    sousId: 82,
+    tokenName: 'BABI',
+    image: 'BABI.svg',
+    stakingTokenName: 'GNANA',
+    stakingTokenAddress: {
+      97: '0x4fb99590ca95fc3255d9fa66a1ca46c43c34b09a',
+      56: '0xddb3bd8645775f59496c821e4f55a7ea6a6dc299',
+    },
+    contractAddress: {
+      97: '0x6c6248ea3ff05200b9df0bc91cec2dea4f57a95d',
+      56: '0xbc2329abb85e0a3dc7e5bfd01901374bd4162831',
+    },
+    poolCategory: PoolCategory.APEZONE,
+    projectLink: 'https://babylons.io/',
+    harvest: true,
+    tokenPerBlock: `1.860119047619047619`,
+    sortOrder: 1,
+    reflect: false,
+    isFinished: false,
+    tokenDecimals: 18,
+  },
   /* {
     sousId: 45,
     tokenName: 'LINA',
