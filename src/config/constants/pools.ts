d7364537029ed0a8ec03e17d168f9a3e6a5c0983
Ixm 759 <759ixm@gmail.com>
2021-08-26 13:30:26 -0500

    feat: Polyplay pool

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -1914,6 +1914,28 @@ const pools: PoolConfig[] = [
     isFinished: false,
     tokenDecimals: 18,
   },
+  {
+    sousId: 87,
+    tokenName: 'PLAY',
+    image: 'PLAY.png',
+    stakingTokenName: 'BANANA',
+    stakingTokenAddress: {
+      97: '0x4fb99590ca95fc3255d9fa66a1ca46c43c34b09a',
+      56: '0x603c7f932ED1fc6575303D8Fb018fDCBb0f39a95',
+    },
+    contractAddress: {
+      97: '0x6c6248ea3ff05200b9df0bc91cec2dea4f57a95d',
+      56: '0x9a3077F34cC30F9BF8E93A0369119bae0113d9cC',
+    },
+    poolCategory: PoolCategory.APEZONE,
+    projectLink: 'https://polyplay.net/',
+    harvest: true,
+    tokenPerBlock: `0.011574074074074074`,
+    sortOrder: 1,
+    reflect: false,
+    isFinished: false,
+    tokenDecimals: 18,
+    }
 ]
 
 export default pools
