1811825e73414062959fbce764fb27056c4936a5
defifofum <defifofum0@gmail.com>
2021-09-05 13:52:52 -0400

    feat: Add KRW pool - Still need an icon

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -2069,6 +2069,28 @@ const pools: PoolConfig[] = [
     isFinished: false,
     tokenDecimals: 18,
   },
+  {
+    sousId: 94,
+    tokenName: 'KRW',
+    image: 'KRW.png',
+    stakingTokenName: 'BANANA',
+    stakingTokenAddress: {
+      97: '0x4fb99590ca95fc3255d9fa66a1ca46c43c34b09a',
+      56: '0x603c7f932ED1fc6575303D8Fb018fDCBb0f39a95',
+    },
+    contractAddress: {
+      97: '0x6c6248ea3ff05200b9df0bc91cec2dea4f57a95d',
+      56: '0x272d18f546CFF72D1a5bBcA9e0E25F97A71FcA1a',
+    },
+    poolCategory: PoolCategory.APEZONE,
+    projectLink: 'https://kingdefi.io/',
+    harvest: true,
+    tokenPerBlock: `14.467592592592592592`,
+    sortOrder: 1,
+    reflect: false,
+    isFinished: false,
+    tokenDecimals: 18,
+  },
 ]
 
 export default pools
