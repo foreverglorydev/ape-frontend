8b82ba93b619d8a45c91dd6cf42ead59b5dc458d
harambe-nakamoto <harambeN@protonmail.com>
2021-09-10 12:48:45 -0500

    chore: Refactor homepage for pools

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import {
   useFetchStats,
   useFetchPublicData,
   useFetchStatsOverall,
-  useStatsOverall,
   useFetchAuctions,
   useFetchTokenPrices,
 } from 'state/hooks'
@@ -72,8 +71,6 @@ const App: React.FC = () => {
   useFetchStatsOverall()
   useFetchAuctions()
 
-  const { statsOverall } = useStatsOverall()
-
   const scrollToTop = (): void => {
     window.scrollTo({
       top: 0,
@@ -94,7 +91,9 @@ const App: React.FC = () => {
             <Route path="/" exact>
               <Home />
             </Route>
-            <Route path="/farms">{statsOverall && <Farms />}</Route>
+            <Route path="/farms">
+              <Farms />
+            </Route>
             <Route path="/pools">
               <Pools />
             </Route>
