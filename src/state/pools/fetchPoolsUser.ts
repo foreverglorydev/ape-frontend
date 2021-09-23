fc1678b1ccdc25d96fd838f65d23f0dc5f54415c
TechLord <tannai.odin@gmail.com>
2021-09-23 05:23:58 -0400

    fix: Typos and wrong imports

--- src/state/pools/fetchPoolsUser.ts
+++ src/state/pools/fetchPoolsUser.ts
@@ -17,7 +17,7 @@ const nonBnbPools = poolsConfig.filter((p) => p.stakingToken.address !== QuoteTo
 const bnbPools = poolsConfig.filter((p) => p.stakingToken.address === QuoteToken.BNB)
 const nonMasterPools = poolsConfig.filter((p) => p.sousId !== 0)
 const web3 = getWeb3()
-const masterChefContract = new web3.eth.Contract((masterChefABI as unknown) as AbiItem, getMasterChefAddress())
+const masterChefContract = new web3.eth.Contract(masterChefABI as unknown as AbiItem, getMasterChefAddress())
 
 export const fetchPoolsAllowance = async (account) => {
   const calls = nonBnbPools.map((p) => ({
