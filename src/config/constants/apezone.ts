5b8e85f0b2269e30b12b75de3ffddeca742bef5a
harambe-nakamoto <harambeN@protonmail.com>
2021-09-27 15:00:48 -0500

    chore: Update banner and description

--- src/config/constants/apezone.ts
+++ src/config/constants/apezone.ts
@@ -8,7 +8,7 @@ const ApeZone: { ifos: Ifo[] } = {
       isActive: true,
       name: 'NFTY Network',
       subTitle: 'Decentralized NFT Gating Ecosystem',
-      description: `NFTY Network is a decentralized NFT ecosystem built to facilitate various innovations in the NFT space, unlocking the true potential of NFTs through gated experiences that connect web2 and web3 infrastructures like never before!`,
+      description: `NFTY Network is a decentralized NFT ecosystem built to facilitate various innovations in the NFT space, unlocking the true potential of NFTs through gated experiences that connect Web2 and Web3 infrastructures like never before!`,
       launchDate: 'September 29th',
       launchTime: '17:00 UTC',
       saleAmount: '30,800,000 NFTY',
