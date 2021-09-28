7fd2700aec8a17732d295538733c7497d5a48214
Apeguru <ape.swap@hotmail.com>
2021-09-27 22:57:03 -0300

    feat: Add new events

--- src/hooks/useBid.ts
+++ src/hooks/useBid.ts
@@ -1,6 +1,8 @@
 import { useCallback } from 'react'
 import { useWeb3React } from '@web3-react/core'
 import { bid } from 'utils/callHelpers'
+import { CHAIN_ID } from 'config/constants'
+import track from 'utils/track'
 import { useAuction } from './useContract'
 
 const useBid = () => {
@@ -11,6 +13,15 @@ const useBid = () => {
     async (amount, id) => {
       try {
         const txHash = await bid(auctionContract, amount, id, account)
+        track({
+          event: 'nfa',
+          chain: CHAIN_ID,
+          data: {
+            id,
+            cat: 'bid',
+            amount,
+          },
+        })
         console.info(txHash)
       } catch (e) {
         console.log(e)
