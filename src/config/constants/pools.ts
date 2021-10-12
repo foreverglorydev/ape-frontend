43bf39b1808d5d280b94f8bb52b0caa98e3c3255
Ixm 759 <759ixm@gmail.com>
2021-10-12 16:03:42 -0500

    feat: THG pool & farm

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -2333,6 +2333,25 @@ const pools: PoolConfig[] = [
     isFinished: false,
     tokenDecimals: 18,
   },
+  {
+    sousId: 121,
+    tokenName: 'THG',
+    image: 'THG.png',
+    stakingToken: tokens.banana,
+    rewardToken: tokens.thg,
+    contractAddress: {
+      97: '0x6c6248ea3ff05200b9df0bc91cec2dea4f57a95d',
+      56: '0xcf44a7c1d8277b27c47195f0907a5b93e63d3dd0',
+    },
+    poolCategory: PoolCategory.APEZONE,
+    projectLink: 'https://thetanarena.com/',
+    harvest: true,
+    tokenPerBlock: `0.025462962962962962`,
+    sortOrder: 1,
+    reflect: false,
+    isFinished: false,
+    tokenDecimals: 18,
+  },
   //
   // PUT ALL ADMIN POOLS UNDER HERE
   //
