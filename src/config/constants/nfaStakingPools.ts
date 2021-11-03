b6ecb042a6c13df0019697e9905527da5e88e8e4
Apeguru <ape.swap@hotmail.com>
2021-11-02 22:20:05 -0300

    chore: Updated NFA Staking pools

--- src/config/constants/nfaStakingPools.ts
+++ src/config/constants/nfaStakingPools.ts
@@ -8,7 +8,7 @@ const nftStakingPools: NfaStakingPoolConfig[] = [
     rewardToken: tokens.banana,
     contractAddress: {
       97: '0x12D87c577738F53b85fEE44738420070C7406e4b',
-      56: '0xC84E54e4e09FfD7f971C674f87002A54A2D6B5E3',
+      56: '0x16b5cdf4931eb5b6e137ff1a6178d723a112ccb4',
     },
     tokenPerBlock: '0',
     isFinished: false,
@@ -18,7 +18,7 @@ const nftStakingPools: NfaStakingPoolConfig[] = [
     tier: 2,
     rewardToken: tokens.banana,
     contractAddress: {
-      97: '0x25e682EdB9641bacecc9271F6de377C69c8B8d0B',
+      97: '0x502e746557362963d5285912A94df8fCB4D46F4D',
       56: '0xa6824F494D26BFB8bAae9cC95c6e01A3657E44d0',
     },
     tokenPerBlock: '0',
@@ -29,7 +29,7 @@ const nftStakingPools: NfaStakingPoolConfig[] = [
     tier: 3,
     rewardToken: tokens.banana,
     contractAddress: {
-      97: '0xc58044A3aB2ED5BBD8F3e15bBB4571A45ad7C6c2',
+      97: '0xEF3E2FfD74a3C3277fdd5D91FCA6711eC4a63E71',
       56: '0x00ee52127D42322E8bbfC10BE6E129b47CCdaE0A',
     },
     tokenPerBlock: '0',
@@ -40,7 +40,7 @@ const nftStakingPools: NfaStakingPoolConfig[] = [
     tier: 4,
     rewardToken: tokens.banana,
     contractAddress: {
-      97: '0xDC4A95CBA750f0F44438187A301BD617A577164a',
+      97: '0x012c0048084950108dfae4402b9d5f67a89d5a9e',
       56: '0x98fBbC7Cb6B02E66Bd2c294D0a69D13e67afB2f7',
     },
     tokenPerBlock: '0',
@@ -51,7 +51,7 @@ const nftStakingPools: NfaStakingPoolConfig[] = [
     tier: 5,
     rewardToken: tokens.banana,
     contractAddress: {
-      97: '0xb675449fbC57BE8b15e0C3F3F7E196650c2441dc',
+      97: '0x834539f712392cf0ccD5b3BBdc0749be42f0c0DE',
       56: '0xec1eD44688d7E5A622656b699eD1339e70890385',
     },
     tokenPerBlock: '0',
