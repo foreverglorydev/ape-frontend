e15de820028f34a586fe51f53b0a63e13aae012e
harambe-nakamoto <harambeN@protonmail.com>
2021-11-16 14:51:31 -0600

    chore: Fix stats page rendering

--- src/views/Stats/components/CardStats.tsx
+++ src/views/Stats/components/CardStats.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Card, CardBody, Heading, Text, Flex, Image, useMatchBreakpoints } from '@apeswapfinance/uikit'
+import { Card, CardBody, Heading, Text, Flex } from '@apeswapfinance/uikit'
 import styled from 'styled-components'
 import { FarmPool } from 'state/types'
 import useI18n from 'hooks/useI18n'
@@ -8,6 +8,7 @@ import ExpandableSectionButton from 'components/ExpandableSectionButton'
 import { useAllPools } from 'state/hooks'
 import DetailsSection from './DetailsSection'
 import CardValue from './CardValue'
+import StatsImageCard from './StatsImageCard'
 
 export interface PoolStatsProps {
   data?: FarmPool
@@ -38,53 +39,8 @@ const ExpandingWrapper = styled.div<{ expanded: boolean }>`
   overflow: hidden;
 `
 
-const IconImage = styled(Image)`
-  align: center;
-  width: 40px;
-  height: 40px;
-
-  ${({ theme }) => theme.mediaQueries.sm} {
-    width: 70px;
-    height: 70px;
-  }
-`
-
-const IconQuoteToken = styled(Image)`
-  align: center;
-  width: 20px;
-  height: 20px;
-
-  ${({ theme }) => theme.mediaQueries.sm} {
-    width: 35px;
-    height: 35px;
-  }
-`
-
-const IconArrow = styled(Image)`
-  align: center;
-  width: 5px;
-  height: 5px;
-
-  ${({ theme }) => theme.mediaQueries.sm} {
-    width: 10px;
-    height: 10px;
-  }
-`
-
-const StyledBackground = styled.div`
-  width: 150px;
-  height: 110px;
-  border-radius: 20px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-right: 5px;
-  background: rgb(255, 179, 0, 0.4);
-`
-
 const CardStats: React.FC<PoolStatsProps> = ({ data, type, forceDetails = false }) => {
   const TranslateString = useI18n()
-  const { isXl: isDesktop } = useMatchBreakpoints()
   const pools = useAllPools()
   const bscScanAddress = `https://bscscan.com/address/${data.address}`
   let farmName = data.name
@@ -104,41 +60,12 @@ const CardStats: React.FC<PoolStatsProps> = ({ data, type, forceDetails = false 
     <StyledPoolStats key={farmName} isActive={type === 'pool'} isSuccess={type === 'farm'}>
       <CardBody>
         <Flex justifyContent="flex-start" alignItems="center" marginBottom="12px">
-          {type === 'pool' ? (
-            <Image
-              src={type === 'pool' ? `/images/tokens/${farmImage}` : `/images/farms/${farmImage}.svg`}
-              alt={farmImage}
-              width={64}
-              height={64}
-            />
-          ) : (
-            <StyledBackground>
-              <IconImage
-                src={`/images/tokens/${farmImage || `${filteredFarm?.tokenSymbol}.svg`}`}
-                alt={filteredFarm?.tokenSymbol}
-                width={50}
-                height={50}
-                marginLeft="7.5px"
-              />
-              <IconQuoteToken
-                src={`/images/tokens/${filteredFarm?.quoteTokenSymbol}.svg`}
-                alt={filteredFarm?.quoteTokenSymbol}
-                width={30}
-                height={30}
-                marginLeft={isDesktop ? '-20px' : '-13px'}
-                marginTop={isDesktop ? '45px' : '30px'}
-              />
-              <IconArrow
-                src="/images/arrow.svg"
-                alt="arrow"
-                width={10}
-                height={10}
-                marginRight="8px"
-                marginLeft="8px"
-              />
-              <IconImage src="/images/tokens/banana.svg" alt="banana" width={50} height={50} marginRight="7.5px" />
-            </StyledBackground>
-          )}
+          <StatsImageCard
+            type={type}
+            image={farmImage}
+            token0={filteredFarm?.quoteTokenSymbol}
+            token1={filteredFarm?.tokenSymbol}
+          />
           <Heading fontSize="16px" mb="24px" style={{ textAlign: 'center', marginLeft: 20 }}>
             {TranslateString(534, `${farmName}`)}
           </Heading>
