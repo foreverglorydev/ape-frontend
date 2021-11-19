75f945842f74c2e4ee18a52122ffbba6a46c6c92
Obie Dobo <obiedobo@gmail.com>
2021-11-19 16:15:15 -0500

    feat: Relay farm pool

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -2533,6 +2533,23 @@ const farms: FarmConfig[] = [
     projectLink: 'https://frax.finance/',
   },
   {
+    pid: 158,
+    image: 'RELAY.svg',
+    lpSymbol: 'RELAY-BNB LP',
+    lpAddresses: {
+      97: '0xed89477d619c7e73f752d5fc7be60308ceb63663',
+      56: '0xab29d19dc9a32fe37275371f0ddfeea3d0c72c25',
+    },
+    tokenSymbol: 'RELAY',
+    tokenAddresses: {
+      97: '0x4Fb99590cA95fc3255D9fA66a1cA46c43C34b09a',
+      56: '0xE338D4250A4d959F88Ff8789EaaE8c32700BD175',
+    },
+    quoteTokenSymbol: QuoteToken.BNB,
+    quoteTokenAdresses: contracts.wbnb,
+    projectLink: 'https://www.relaychain.com/',
+  },
+  {
     pid: 7, // NOTE: Inactive Farm
     image: 'BAKE.svg',
     lpSymbol: 'BAKE-BANANA',
