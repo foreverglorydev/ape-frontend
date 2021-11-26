de218df79691d973a003cba20c6bd5ebc272d95d
Baba Drape <babadrape@gmail.com>
2021-11-26 07:53:15 +0100

    add relevant modifications to iao banner

--- src/views/Ifos/index.tsx
+++ src/views/Ifos/index.tsx
@@ -9,10 +9,8 @@ import CurrentIfo from './CurrentIfo'
 import PastIfo from './PastIfo'
 
 const Header = styled.div`
-  position: relative;
-  overflow-y: hidden;
-  overflow-x: hidden;
-  padding-top: 36px;
+  display: flex;
+  width: 100%;
   background-image: url(/images/banners/iao-bg.svg);
   height: 250px;
   background-size: cover;
@@ -32,24 +30,20 @@ const Header = styled.div`
     padding-right: 10px;
   }
 `
-
 const HeadingContainer = styled.div`
-  max-width: 1024px;
-  margin-left: auto;
-  margin-right: auto;
-  position: relative;
-  padding-left: 10px;
+  position: absolute;
   z-index: 999;
-`
+  padding-top: 36px;
+  padding-left: 10px;
+  height: inherit;
 
+  ${({ theme }) => theme.mediaQueries.md} {
+    position: static;
+  }
+`
 const StyledHeading = styled(Heading)`
   font-size: 32px;
   max-width: 176px !important;
-
-  ${({ theme }) => theme.mediaQueries.xs} {
-    font-size: 36px;
-    max-width: 240px !important;
-  }
 
   ${({ theme }) => theme.mediaQueries.md} {
     font-size: 44px;
@@ -61,26 +55,14 @@ const StyledHeading = styled(Heading)`
     max-width: 600px !important;
   }
 `
-
 const RightDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100%;
-  height: 100%;
+  height: inherit;
   max-width: 800px;
-  position: absolute;
-  top: 0;
-  right: 0;
-  /* background-color: red; */
-
-  ${({ theme }) => theme.mediaQueries.md} {
-    width: 80%;
-    margin: 0px auto;
-  }
-
-  ${({ theme }) => theme.mediaQueries.lg} {
-    /* width: 800px; */
-  }
-
-  /* do for screen size 768px */
+  overflow-y: hidden;
 `
 
 const apeFloat = keyframes`
@@ -90,33 +72,28 @@ const apeFloat = keyframes`
 `
 
 const Ape = styled.img`
-  /* background-color: yellow; */
   width: 300px;
-  margin-top: 100px;
-  position: absolute;
-  right: -30px;
+  margin-top: 250px;
+  margin-left: 20%;
   animation: ${apeFloat} 10s ease-in-out infinite;
 
   ${({ theme }) => theme.mediaQueries.md} {
-    width: 400px;
-    right: 20px;
+    width: 450px;
+    margin-top: 350px;
   }
 
-  ${({ theme }) => theme.mediaQueries.lg} {
-    width: 500px;
+  ${({ theme }) => theme.mediaQueries.md} {
+    width: 450px;
   }
 `
-
-const WindowImage = styled.div`
-  /* background-color: green; */
+const WindowDiv = styled.div`
+  position: absolute;
   background-image: url(/images/banners/iao-window.svg);
   background-size: cover;
   background-repeat: no-repeat;
   height: inherit;
   background-position: inherit;
   width: 100%;
-  position: absolute;
-  top: 0;
 `
 
 const bananaFloat = keyframes`
@@ -126,7 +103,6 @@ const bananaFloat = keyframes`
 `
 
 const Banana = styled.img`
-  /* background-color: blue; */
   width: 100px;
   height: auto;
   position: absolute;
@@ -134,8 +110,14 @@ const Banana = styled.img`
   right: 0;
   animation: ${bananaFloat} 10s linear infinite;
 
+  ${({ theme }) => theme.mediaQueries.sm} {
+    margin-right: 100px;
+  }
+
   ${({ theme }) => theme.mediaQueries.md} {
     width: 150px;
+    top: 0;
+    margin-right: 60px;
   }
 
   ${({ theme }) => theme.mediaQueries.lg} {
@@ -143,79 +125,13 @@ const Banana = styled.img`
   }
 `
 
-const Hd = styled.div`
-  display: flex;
-  width: 100%;
-  background-image: url(/images/banners/iao-bg.svg);
-  height: 250px;
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  margin-bottom: 32px;
-`
-const Hc = styled.div`
-  position: absolute;
-  z-index: 999;
-  padding-top: 36px;
-  padding-left: 10px;
-  height: inherit;
-`
-const Sh = styled(Heading)`
-  font-size: 32px;
-  max-width: 176px !important;
-
-  ${({ theme }) => theme.mediaQueries.xs} {
-    font-size: 36px;
-    max-width: 240px !important;
-  }
-
-  ${({ theme }) => theme.mediaQueries.md} {
-    font-size: 44px;
-    max-width: 400px !important;
-  }
-
-  ${({ theme }) => theme.mediaQueries.xl} {
-    font-size: 60px;
-    max-width: 600px !important;
-  }
-`
-const Rd = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: inherit;
-  max-width: 800px;
-  overflow-y: hidden;
-`
-const Ap = styled.img`
-  width: 300px;
-  margin-top: 80%;
-  margin-left: 20%;
-  animation: ${apeFloat} 10s ease-in-out infinite;
-`
-const WiDiv = styled.div`
-  background-color: red;
-  position: absolute;
-  background-image: url(/images/banners/iao-window.svg);
-  background-size: cover;
-  background-repeat: no-repeat;
-  height: inherit;
-  background-position: inherit;
-  width: 100%;
-`
-// const Wi = styled.img`
-//   width: 100%;
-//   height: auto;
-// `
-
 const Ifos = () => {
   const { path } = useRouteMatch()
   const TranslateString = useI18n()
 
   return (
     <>
-      {/* <Header>
+      <Header>
         <HeadingContainer>
           <StyledHeading as="h1" mb="8px" mt={0} color="white">
             {TranslateString(999, 'Initial Ape Offerings')}
@@ -223,26 +139,11 @@ const Ifos = () => {
         </HeadingContainer>
 
         <RightDiv>
-          <WindowImage className="window">
-            <Ape src="/images/banners/iao-ape.svg" className="ape" />
-          </WindowImage>
+          <Ape src="/images/banners/iao-ape.svg" className="ape" />
+          <WindowDiv className="window" />
           <Banana src="/images/banners/iao-banana.svg" className="banana" />
         </RightDiv>
-      </Header> */}
-
-      <Hd>
-        <Hc>
-          <Sh as="h1" mb="8px" mt={0} color="white">
-            {TranslateString(999, 'Initial Ape Offerings')}
-          </Sh>
-        </Hc>
-
-        <Rd>
-          <Ap src="/images/banners/iao-ape.svg" className="ape" />
-          <WiDiv className="window">{/* <Wi src="/images/banners/iao-window.svg" className="ape" /> */}</WiDiv>
-          <Banana src="/images/banners/iao-banana.svg" className="banana" />
-        </Rd>
-      </Hd>
+      </Header>
 
       <Container>
         <IfoTabButtons />
