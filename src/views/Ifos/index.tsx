e09672634660ed410f4ad821f6e2c73ab52556ae
Baba Drape <babadrape@gmail.com>
2021-11-28 12:12:07 +0100

    redo sm screen

--- src/views/Ifos/index.tsx
+++ src/views/Ifos/index.tsx
@@ -17,9 +17,10 @@ const Header = styled.div`
   background-repeat: no-repeat;
   background-position: center;
   margin-bottom: 32px;
+  padding-top: 36px;
   overflow: hidden;
 
-  ${({ theme }) => theme.mediaQueries.md} {
+  /* ${({ theme }) => theme.mediaQueries.md} {
     height: 300px;
     padding-left: 24px;
     padding-right: 24px;
@@ -29,7 +30,7 @@ const Header = styled.div`
     height: 300px;
     padding-left: 10px;
     padding-right: 10px;
-  }
+  } */
 `
 const HeadingContainer = styled.div`
   position: absolute;
@@ -126,14 +127,92 @@ const Banana = styled.img`
   }
 `
 
+const Hdr = styled.div`
+  position: relative;
+  overflow-y: hidden;
+  overflow-x: hidden;
+  padding-top: 36px;
+  padding-left: 10px;
+  padding-right: 10px;
+  background-image: url(/images/banners/iao-bg.svg);
+  height: 250px;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+`
+const Hc = styled.div`
+  max-width: 1024px;
+  margin-left: auto;
+  margin-right: auto;
+  position: absolute;
+  z-index: 999;
+`
+const Sh = styled(Heading)`
+  font-size: 36px;
+  max-width: 240px !important;
+`
+
+const Rd = styled.div`
+  background-color: pink;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  bottom: 0px;
+  right: 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .ele-con {
+    position: relative;
+    width: 100%;
+    height: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`
+
+const Ap = styled.img`
+  width: 300px;
+  margin-top: 15em;
+  margin-left: 80px;
+  animation: ${apeFloat} 10s ease-in-out infinite;
+`
+
+const WinDiv = styled.div`
+  background-image: url(/images/banners/iao-window.svg);
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: absolute;
+  top: 0;
+`
+
 const Ifos = () => {
   const { path } = useRouteMatch()
   const TranslateString = useI18n()
 
   return (
     <>
-      <Header>
-        <HeadingContainer>
+      <Hdr>
+        <Hc>
+          <Sh as="h1" mb="8px" mt={0} color="white">
+            {TranslateString(999, 'Initial Ape Offerings')}
+          </Sh>
+        </Hc>
+
+        <Rd>
+          <Ap src="/images/banners/iao-ape.svg" className="ape" />
+          <WinDiv className="window" />
+          {/* <div className="ele-con">
+          </div> */}
+        </Rd>
+
+        {/* <HeadingContainer>
           <StyledHeading as="h1" mb="8px" mt={0} color="white">
             {TranslateString(999, 'Initial Ape Offerings')}
           </StyledHeading>
@@ -143,8 +222,8 @@ const Ifos = () => {
           <Ape src="/images/banners/iao-ape.svg" className="ape" />
           <WindowDiv className="window" />
           <Banana src="/images/banners/iao-banana.svg" className="banana" />
-        </RightDiv>
-      </Header>
+        </RightDiv> */}
+      </Hdr>
 
       <Container>
         <IfoTabButtons />
