14b2482c2f081d608938568aa52c26c96b4a0bd1
harambe-nakamoto <harambeN@protonmail.com>
2021-11-29 15:36:54 -0600

    chore: Add mobile banners

--- src/views/Iazos/components/Header.tsx
+++ src/views/Iazos/components/Header.tsx
@@ -10,15 +10,21 @@ const HeaderWrapper = styled.div`
   width: 100%;
   padding-top: 36px;
   background-image: ${(props) =>
-    props.theme.isDark ? 'url(/images/iazo-banner-dark.svg)' : 'url(/images/iazo-banner-light.svg)'};
+    props.theme.isDark ? 'url(/images/ss-iao-mobile-night.svg)' : 'url(/images/ss-iao-mobile.svg)'};
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
   display: flex;
   justify-content: center;
   align-items: center;
-  ${({ theme }) => theme.mediaQueries.lg} {
+  ${({ theme }) => theme.mediaQueries.sm} {
+    background-image: ${(props) =>
+      props.theme.isDark ? 'url(/images/ss-iao-tablet-night.svg)' : 'url(/images/ss-iao-tablet.svg)'};
+  }
+  ${({ theme }) => theme.mediaQueries.xl} {
     height: 300px;
+    background-image: ${(props) =>
+      props.theme.isDark ? 'url(/images/ss-iao-dark.svg)' : 'url(/images/ss-iao-light.svg)'};
   }
 `
 
@@ -32,11 +38,19 @@ const HeadingText = styled(Text)`
   font-family: Titan One;
   font-style: normal;
   font-weight: normal;
-  font-size: 40px;
+  font-size: 28px;
   line-height: 57px;
   text-align: center;
   letter-spacing: 0.05em;
-  top: 5px;
+  top: 40px;
+  left: -45px;
+  ${({ theme }) => theme.mediaQueries.sm} {
+    top: 80px;
+    margin-right: 525px;
+    width: 585px;
+    height: 80px;
+    font-size: 40px;
+    line-height: 20px;
   ${({ theme }) => theme.mediaQueries.xl} {
     top: 80px;
     margin-right: 525px;
@@ -44,6 +58,7 @@ const HeadingText = styled(Text)`
     height: 80px;
     font-size: 60px;
     line-height: 20px;
+    left: 250px;
   }
 `
 
