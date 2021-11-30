f1ac8b1bfd2eaa1714a19e4996dc9734f59b0418
Harambe-Nakamoto <80497939+Harambe-Nakamoto@users.noreply.github.com>
2021-11-30 11:23:24 -0600

    Better default update
    Update dark to default

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -9,7 +9,7 @@ const ThemeContext = React.createContext({ isDark: null, toggleTheme: () => null
 const ThemeContextProvider = ({ children }) => {
   const [isDark, setIsDark] = useState(() => {
     const isDarkUserSetting = localStorage.getItem(CACHE_KEY)
-    return isDarkUserSetting ? JSON.parse(isDarkUserSetting) : false
+    return isDarkUserSetting ? JSON.parse(isDarkUserSetting) : true
   })
 
   const toggleTheme = () => {
@@ -21,7 +21,7 @@ const ThemeContextProvider = ({ children }) => {
 
   return (
     <ThemeContext.Provider value={{ isDark, toggleTheme }}>
-      <SCThemeProvider theme={!isDark ? dark : light}>{children}</SCThemeProvider>
+      <SCThemeProvider theme={isDark ? dark : light}>{children}</SCThemeProvider>
     </ThemeContext.Provider>
   )
 }
