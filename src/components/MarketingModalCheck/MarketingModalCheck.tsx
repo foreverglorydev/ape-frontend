b9534fdd8a1b3226ed9ecf1125fb4c8b30fc39e8
Baba Drape <babadrape@gmail.com>
2021-12-05 20:17:29 +0100

    add pathname to route on close modal

--- src/components/MarketingModalCheck/MarketingModalCheck.tsx
+++ src/components/MarketingModalCheck/MarketingModalCheck.tsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import { useLocation, useHistory } from 'react-router-dom'
-import { MarketingModal } from '@apeswapfinance/uikit'
+import { MarketingModal, useWalletModal } from '@apeswapfinance/uikit'
+import useAuth from 'hooks/useAuth'
 
 const MarketingModalCheck = () => {
   const location = useLocation()
   const history = useHistory()
+  const { login, logout } = useAuth()
 
+  const { onPresentConnectModal } = useWalletModal(login, logout)
   const correctDisplayRoute = location.search.includes('modal=1')
 
   const onDismiss = () => {
     history.push({
-      pathname: '/',
+      pathname: location.pathname,
     })
+  }
+
+  const openFarmsLink = () => {
+    return window.open('https://apeswap.finance/farms', '_blank')
+  }
+
+  const openLiquidityLink = () => {
+    return window.open('https://app.apeswap.finance/add', '_blank')
   }
 
   return (
@@ -20,10 +31,10 @@ const MarketingModalCheck = () => {
         title="Welcome to ApeSwap's Farms"
         description="Start earning passive income with your cryptocurrency!"
         onDismiss={onDismiss}
-        goToFarms={() => null}
-        goToLiquidity={() => null}
-        connectWallet={() => null}
-        startEarning={() => null}
+        goToFarms={openFarmsLink}
+        goToLiquidity={openLiquidityLink}
+        connectWallet={onPresentConnectModal}
+        startEarning={onDismiss}
       />
     )
   )
