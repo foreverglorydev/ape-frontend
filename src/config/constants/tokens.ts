8afe5c6f1a23e4b6f6e0c5b1e1db7cad6a949ee8
Obie Dobo <obiedobo@gmail.com>
2021-12-10 22:22:29 -0600

    feat: STZ logo update

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -815,9 +815,9 @@ const tokens = {
     decimals: 18,
   },
   stz: {
-    symbol: "STZ",
+    symbol: 'STZ',
     address: {
-      56: "0x7FE378c5e0B5C32AF2ecc8829beDF02245A0E4eF",
+      56: '0x7FE378c5e0B5C32AF2ecc8829beDF02245A0E4eF',
     },
     decimals: 18,
   },
