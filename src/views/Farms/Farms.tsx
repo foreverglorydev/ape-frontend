3065b0717fe5e21f056c1d48728fd3ed07dbcfa2
apewukong <apewukong@users.noreply.github.com>
2022-01-25 20:52:01 -0500

    WIP style update

--- src/views/Farms/Farms.tsx
+++ src/views/Farms/Farms.tsx
@@ -3,10 +3,8 @@ import { Route, useRouteMatch, useLocation } from 'react-router-dom'
 import BigNumber from 'bignumber.js'
 import { getBalanceNumber } from 'utils/formatBalance'
 import { useWeb3React } from '@web3-react/core'
-import { Heading, RowType, Text, Card, Checkbox, ArrowDropDownIcon } from '@apeswapfinance/uikit'
-import styled from 'styled-components'
+import { RowType } from '@apeswapfinance/uikit'
 import { BLOCKS_PER_YEAR, BANANA_PER_BLOCK, BANANA_POOL_PID } from 'config'
-import Page from 'components/layout/Page'
 import {
   useFarms,
   usePriceBnbBusd,
@@ -30,380 +28,7 @@ import SearchInput from './components/SearchInput'
 import { RowProps } from './components/FarmTable/Row'
 import ToggleView from './components/ToggleView/ToggleView'
 import { DesktopColumnSchema, ViewMode } from './components/types'
-
-interface LabelProps {
-  active?: boolean
-}
-
-const ControlContainer = styled(Card)`
-  display: flex;
-  width: 100%;
-  align-items: center;
-  position: relative;
-
-  justify-content: center;
-  flex-direction: column;
-  overflow: visible;
-  padding-bottom: 10px;
-  transform: translateY(-85px);
-
-  ${({ theme }) => theme.mediaQueries.md} {
-    flex-direction: row;
-    height: 59px;
-    padding: 0px;
-    justify-content: flex-start;
-    padding-left: 50px;
-    transform: translateY(-60px);
-  }
-`
-
-const ToggleWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-left: 10px;
-  cursor: pointer;
-
-  ${Text} {
-    margin-left: 4px;
-    ${({ theme }) => theme.mediaQueries.md} {
-      margin-left: 8px;
-    }
-  }
-`
-
-const LabelWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  > ${Text} {
-    font-size: 12px;
-  }
-
-  margin-left: 30px;
-
-  ${({ theme }) => theme.mediaQueries.md} {
-    flex-direction: row;
-    margin-left: 0px;
-    align-items: center;
-  }
-`
-
-const ViewControls = styled.div`
-  flex-wrap: wrap;
-  justify-content: flex-start;
-  display: flex;
-  align-items: flex-end;
-  width: 100%;
-
-  > div {
-    padding: 8px 0px;
-  }
-
-  ${({ theme }) => theme.mediaQueries.md} {
-    justify-content: center;
-    align-items: center;
-    width: auto;
-
-    > div {
-      padding: 0;
-    }
-  }
-`
-
-const HeadingContainer = styled.div`
-  max-width: 1024px;
-  margin-left: auto;
-  margin-right: auto;
-`
-
-const Header = styled.div`
-  padding-top: 36px;
-  padding-left: 10px;
-  padding-right: 10px;
-  background-image: ${({ theme }) => (theme.isDark ? 'url(/images/farm-night.svg)' : 'url(/images/farm-day.svg)')};
-  background-repeat: no-repeat;
-  background-size: cover;
-  height: 250px;
-  background-position: center;
-
-  ${({ theme }) => theme.mediaQueries.md} {
-    padding-left: 24px;
-    padding-right: 24px;
-    height: 300px;
-  }
-`
-
-const StyledText = styled(Text)`
-  font-weight: 700;
-  font-size: 12px;
-
-  ${({ theme }) => theme.mediaQueries.lg} {
-    font-size: 15px !important;
-  }
-`
-
-interface CheckboxProps {
-  checked?: boolean
-}
-
-const StyledCheckbox = styled(Checkbox)<CheckboxProps>`
-  height: 21px;
-  width: 21px;
-`
-
-const StyledImage = styled.img`
-  height: 187px;
-  width: 134px;
-  position: absolute;
-  right: 0px;
-  bottom: 51px;
-
-  @media screen and (min-width: 340px) {
-    right: 20px;
-  }
-
-  ${({ theme }) => theme.mediaQueries.xs} {
-    bottom: 51px;
-    right: 0px;
-  }
-
-  ${({ theme }) => theme.mediaQueries.md} {
-    bottom: 0px;
-    right: 0px;
-  }
-`
-
-const ContainerLabels = styled.div`
-  background: ${({ theme }) => theme.card.background};
-  border-radius: 16px;
-  margin-top: 24px;
-  height: 32px;
-  width: 100%;
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  transform: translateY(-85px);
-
-  ${({ theme }) => theme.mediaQueries.xs} {
-    margin-top: 34px;
-  }
-
-  ${({ theme }) => theme.mediaQueries.md} {
-    transform: translateY(-60px);
-  }
-`
-
-const StyledLabelContainerHot = styled.div`
-  cursor: pointer;
-  ${({ theme }) => theme.mediaQueries.xs} {
-    margin-left: 5px;
-    margin-right: 5px;
-  }
-  ${({ theme }) => theme.mediaQueries.sm} {
-    margin-left: 15px;
-    margin-right: 15px;
-  }
-  ${({ theme }) => theme.mediaQueries.md} {
-    margin-left: 35px;
-    margin-right: 35px;
-  }
-
-  ${({ theme }) => theme.mediaQueries.lg} {
-    position: absolute;
-    top: 6px;
-    left: 38px;
-    margin: 0px;
-  }
-`
-
-const StyledLabelContainerLP = styled.div`
-  ${({ theme }) => theme.mediaQueries.xs} {
-    margin-left: 5px;
-    margin-right: 5px;
-  }
-  ${({ theme }) => theme.mediaQueries.sm} {
-    margin-left: 15px;
-    margin-right: 15px;
-  }
-  ${({ theme }) => theme.mediaQueries.md} {
-    margin-left: 35px;
-    margin-right: 35px;
-  }
-  ${({ theme }) => theme.mediaQueries.lg} {
-    position: absolute;
-    top: 6px;
-    left: 169px;
-    margin: 0px;
-  }
-`
-
-const StyledLabelContainerAPR = styled.div`
-  cursor: pointer;
-
-  ${({ theme }) => theme.mediaQueries.xs} {
-    margin-left: 5px;
-    margin-right: 5px;
-  }
-  ${({ theme }) => theme.mediaQueries.sm} {
-    margin-left: 15px;
-    margin-right: 15px;
-  }
-  ${({ theme }) => theme.mediaQueries.md} {
-    margin-left: 35px;
-    margin-right: 35px;
-  }
-  ${({ theme }) => theme.mediaQueries.lg} {
-    position: absolute;
-    top: 6px;
-    left: 365px;
-    margin: 0px;
-  }
-  ${({ theme }) => theme.mediaQueries.xl} {
-    left: 409px;
-  }
-`
-
-const StyledLabelContainerLiquidity = styled.div`
-  cursor: pointer;
-  ${({ theme }) => theme.mediaQueries.xs} {
-    margin-left: 5px;
-    margin-right: 5px;
-  }
-  ${({ theme }) => theme.mediaQueries.sm} {
-    margin-left: 15px;
-    margin-right: 15px;
-  }
-  ${({ theme }) => theme.mediaQueries.md} {
-    margin-left: 35px;
-    margin-right: 35px;
-  }
-  ${({ theme }) => theme.mediaQueries.lg} {
-    position: absolute;
-    top: 6px;
-    left: 500px;
-    margin: 0px;
-  }
-  ${({ theme }) => theme.mediaQueries.xl} {
-    left: 621px;
-  }
-`
-
-const StyledLabelContainerEarned = styled.div`
-  cursor: pointer;
-  ${({ theme }) => theme.mediaQueries.xs} {
-    margin-left: 5px;
-    margin-right: 5px;
-  }
-  ${({ theme }) => theme.mediaQueries.sm} {
-    margin-left: 15px;
-    margin-right: 15px;
-  }
-  ${({ theme }) => theme.mediaQueries.md} {
-    margin-left: 35px;
-    margin-right: 35px;
-  }
-  ${({ theme }) => theme.mediaQueries.lg} {
-    margin: 0px;
-    position: absolute;
-    top: 6px;
-    left: 651px;
-  }
-  ${({ theme }) => theme.mediaQueries.xl} {
-    left: 801px;
-  }
-`
-
-const CardContainer = styled.div`
-  margin-top: 17px;
-
-  transform: translateY(-85px);
-  ${({ theme }) => theme.mediaQueries.md} {
-    transform: translateY(-60px);
-  }
-`
-
-const ButtonCheckWrapper = styled.div`
-  justify-content: space-between;
-  align-items: center;
-  display: flex;
-  width: 100%;
-  margin-right: 30px;
-
-  ${({ theme }) => theme.mediaQueries.md} {
-    width: fit-content;
-  }
-`
-
-const StyledHeading = styled(Heading)`
-  font-size: 30px;
-  max-width: 176px !important;
-
-  ${({ theme }) => theme.mediaQueries.xs} {
-    font-size: 30px;
-    max-width: 240px !important;
-  }
-
-  ${({ theme }) => theme.mediaQueries.md} {
-    font-size: 44px;
-    max-width: 400px !important;
-  }
-
-  ${({ theme }) => theme.mediaQueries.xl} {
-    font-size: 60px;
-    max-width: 600px !important;
-  }
-`
-
-const StyledPage = styled(Page)`
-  padding-left: 5px;
-  padding-right: 5px;
-  width: 100vw;
-
-  ${({ theme }) => theme.mediaQueries.xs} {
-    padding-left: 10px;
-    padding-right: 10px;
-  }
-  ${({ theme }) => theme.mediaQueries.md} {
-    padding-left: 16px;
-    padding-right: 16px;
-  }
-`
-
-const StyledLabel = styled.div<LabelProps>`
-  display: flex;
-  color: ${({ theme, active }) => (active ? '#FFFFFF' : theme.colors.primary)};
-  font-family: Poppins;
-  padding: 4px 12px;
-  font-weight: bold;
-  font-size: 12px;
-  line-height: 12px;
-  border-radius: ${({ active }) => active && '50px'};
-  background-color: ${({ active }) => active && '#FFB300'};
-`
-
-interface DropdownProps {
-  down?: boolean
-}
-
-const StyledArrowDropDownIcon = styled(ArrowDropDownIcon)<DropdownProps>`
-  color: white;
-  transform: ${({ down }) => (!down ? 'rotate(180deg)' : 'rotate(0)')};
-  margin-left: 7px;
-  margin-top: 2px;
-  /* 'rotate(180deg)' : 'rotate(0)'; */
-`
-
-const FlexLayout = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
-  & > * {
-    width: 100%;
-    margin-bottom: 32px;
-  }
-`
+import * as S from './styles'
 
 const NUMBER_OF_FARMS_VISIBLE = 12
 
@@ -649,8 +274,8 @@ const Farms: React.FC = () => {
     }
 
     return (
-      <CardContainer>
-        <FlexLayout>
+      <S.CardContainer>
+        <S.FlexLayout>
           <Route exact path={`${path}`}>
             {farmsStakedMemoized.map((farm) => (
               <FarmCard
@@ -675,8 +300,8 @@ const Farms: React.FC = () => {
               />
             ))}
           </Route>
-        </FlexLayout>
-      </CardContainer>
+        </S.FlexLayout>
+      </S.CardContainer>
     )
   }
 
@@ -694,75 +319,75 @@ const Farms: React.FC = () => {
   return (
     <>
       <MarketingModalCheck />
-      <Header>
-        <HeadingContainer>
-          <StyledHeading as="h1" mb="12px" mt={0} fontFamily="Titan One">
+      <S.Header>
+        <S.HeadingContainer>
+          <S.StyledHeading as="h1" mb="12px" mt={0} fontFamily="Titan One">
             {TranslateString(999, 'Stake LP tokens to earn BANANA')}
-          </StyledHeading>
-        </HeadingContainer>
-      </Header>
+          </S.StyledHeading>
+        </S.HeadingContainer>
+      </S.Header>
 
-      <StyledPage width="1130px">
-        <ControlContainer>
-          <ViewControls>
+      <S.StyledPage width="1130px">
+        <S.ControlContainer>
+          <S.ViewControls>
             {size.width > 968 && viewMode !== null && (
               <ToggleView viewMode={viewMode} onToggle={(mode: ViewMode) => setViewMode(mode)} />
             )}
-            <LabelWrapper>
-              <StyledText mr="15px">Search</StyledText>
+            <S.LabelWrapper>
+              <S.StyledText mr="15px">Search</S.StyledText>
               <SearchInput onChange={handleChangeQuery} value={query} />
-            </LabelWrapper>
-            <ButtonCheckWrapper>
+            </S.LabelWrapper>
+            <S.ButtonCheckWrapper>
               <FarmTabButtons />
-              <ToggleWrapper onClick={() => setStakedOnly(!stakedOnly)}>
-                <StyledCheckbox checked={stakedOnly} onChange={() => setStakedOnly(!stakedOnly)} />
-                <StyledText> {TranslateString(1116, 'Staked')}</StyledText>
-              </ToggleWrapper>
-            </ButtonCheckWrapper>
+              <S.ToggleWrapper onClick={() => setStakedOnly(!stakedOnly)}>
+                <S.StyledCheckbox checked={stakedOnly} onChange={() => setStakedOnly(!stakedOnly)} />
+                <S.StyledText> {TranslateString(1116, 'Staked')}</S.StyledText>
+              </S.ToggleWrapper>
+            </S.ButtonCheckWrapper>
             {isDark ? (
-              <StyledImage src="/images/farm-night-farmer.svg" alt="night-monkey" />
+              <S.StyledImage src="/images/farm-night-farmer.svg" alt="night-monkey" />
             ) : (
-              <StyledImage src="/images/farm-day-farmer.svg" alt="day-monkey" />
+              <S.StyledImage src="/images/farm-day-farmer.svg" alt="day-monkey" />
             )}
-          </ViewControls>
-        </ControlContainer>
-        <ContainerLabels>
-          <StyledLabelContainerHot>
-            <StyledLabel active={sortOption === 'hot'} onClick={() => handleSortOptionChange('hot')}>
+          </S.ViewControls>
+        </S.ControlContainer>
+        <S.ContainerLabels>
+          <S.StyledLabelContainerHot>
+            <S.StyledLabel active={sortOption === 'hot'} onClick={() => handleSortOptionChange('hot')}>
               Hot
-            </StyledLabel>
-          </StyledLabelContainerHot>
-          <StyledLabelContainerLP>
-            <StyledLabel>LP</StyledLabel>
-          </StyledLabelContainerLP>
-          <StyledLabelContainerAPR>
-            <StyledLabel active={sortOption === 'apr'} onClick={() => handleSortOptionChange('apr')}>
+            </S.StyledLabel>
+          </S.StyledLabelContainerHot>
+          <S.StyledLabelContainerLP>
+            <S.StyledLabel>LP</S.StyledLabel>
+          </S.StyledLabelContainerLP>
+          <S.StyledLabelContainerAPR>
+            <S.StyledLabel active={sortOption === 'apr'} onClick={() => handleSortOptionChange('apr')}>
               APR
               {sortOption === 'apr' ? (
-                <StyledArrowDropDownIcon width="7px" height="8px" color="white" down={sortDirection === 'desc'} />
+                <S.StyledArrowDropDownIcon width="7px" height="8px" color="white" down={sortDirection === 'desc'} />
               ) : null}
-            </StyledLabel>
-          </StyledLabelContainerAPR>
-          <StyledLabelContainerLiquidity>
-            <StyledLabel active={sortOption === 'liquidity'} onClick={() => handleSortOptionChange('liquidity')}>
+            </S.StyledLabel>
+          </S.StyledLabelContainerAPR>
+          <S.StyledLabelContainerLiquidity>
+            <S.StyledLabel active={sortOption === 'liquidity'} onClick={() => handleSortOptionChange('liquidity')}>
               Liquidity
               {sortOption === 'liquidity' ? (
-                <StyledArrowDropDownIcon width="7px" height="8px" color="white" down={sortDirection === 'desc'} />
+                <S.StyledArrowDropDownIcon width="7px" height="8px" color="white" down={sortDirection === 'desc'} />
               ) : null}
-            </StyledLabel>
-          </StyledLabelContainerLiquidity>
-          <StyledLabelContainerEarned>
-            <StyledLabel active={sortOption === 'earned'} onClick={() => handleSortOptionChange('earned')}>
+            </S.StyledLabel>
+          </S.StyledLabelContainerLiquidity>
+          <S.StyledLabelContainerEarned>
+            <S.StyledLabel active={sortOption === 'earned'} onClick={() => handleSortOptionChange('earned')}>
               Earned
               {sortOption === 'earned' ? (
-                <StyledArrowDropDownIcon width="7px" height="8px" color="white" down={sortDirection === 'desc'} />
+                <S.StyledArrowDropDownIcon width="7px" height="8px" color="white" down={sortDirection === 'desc'} />
               ) : null}
-            </StyledLabel>
-          </StyledLabelContainerEarned>
-        </ContainerLabels>
+            </S.StyledLabel>
+          </S.StyledLabelContainerEarned>
+        </S.ContainerLabels>
         {viewMode === null ? null : renderContent()}
         <div ref={loadMoreRef} />
-      </StyledPage>
+      </S.StyledPage>
     </>
   )
 }
