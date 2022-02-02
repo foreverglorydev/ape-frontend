98b207adafc6a3126a3a561dafa1e4e3ae9350c7
apewukong <apewukong@users.noreply.github.com>
2022-02-01 21:26:49 -0500

    Remove duplicated project

--- src/config/constants/ifo.ts
+++ src/config/constants/ifo.ts
@@ -1,33 +1,6 @@
 import { Ifo } from './types'
 
 const ifos: Ifo[] = [
-  // TEST
-  // TEST
-  {
-    id: 'trustnft',
-    address: '0xF2a0A19e63cD869550B074a1d435ebd52EDF2240', // Leave empty for "Coming Soon!"
-    isActive: true,
-    isLinear: true,
-    name: 'TrustNFT',
-    subTitle: `Decentralized NFT Loans & Marketplace`,
-    description: `TrustNFT's platform uses AI and big data to accurately evaluate NFTs and unlock their potential for use as loan collateral. Users can combine DeFi with NFTs to earn yield on selected NFT-backed loans. TrustNFT addresses major problems in the NFT ecosystem, including low liquidity, investment risk, and monetization of assets.`,
-    launchDate: 'February 3rd',
-    launchTime: '1:00 UTC',
-    saleAmount: '30,000,000 TRUSTNFT',
-    raiseAmount: '$525,000',
-    vestingTime: '3 Months',
-    projectSiteUrl: 'https://trustnft.org/',
-    currency: 'BNB',
-    currencyAddress: '0x0000000000000000000000000000000000000000',
-    offeringCurrency: 'TRUSTNFT',
-    tokenDecimals: 18,
-    releaseBlockNumber: 22364007, // block to start showing contract details
-    vesting: true,
-    // burnedTxUrl: '',
-  },
-  //
-  //
-  //
   {
     id: 'trustnft',
     address: '', // Leave empty for "Coming Soon!"
