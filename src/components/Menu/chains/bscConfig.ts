37c922c7b7c1d4bfc5cca3f08cf6ad54147bf060
Harambe-Nakamoto <haramben@protonmail.com>
2022-02-08 15:13:50 -0600

    chore: Add lending icon

--- src/components/Menu/chains/bscConfig.ts
+++ src/components/Menu/chains/bscConfig.ts
@@ -58,9 +58,9 @@ const bscConfig: MenuEntry[] = [
     href: '/gnana',
   },
   {
-    label: 'Burn',
-    icon: 'GameBurnIcon',
-    href: '/burn',
+    label: 'Lending',
+    icon: 'LendingIcon',
+    href: 'https://app.ola.finance/apeswap/markets',
   },
   {
     label: 'Info',
